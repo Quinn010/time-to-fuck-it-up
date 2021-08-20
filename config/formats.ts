505dd20d75b32d1b713bd610658ac62b4869456e
Quinn010 <89210947+Quinn010@users.noreply.github.com>
2021-08-20 21:14:34 +0200

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -248,7 +248,7 @@ export const Formats: FormatList = [
 			battle: 24,
 		},
 		// no restrictions, for serious (other than team preview)
-		ruleset: ['Team Preview', 'Cancel Mod'],
+		ruleset: ['Team Preview', 'Cancel Mod', 'Sleep Clause Mod', 'Dynamax Clause', 'Little Cup', 'Evasion Moves Clause' ],
 	},
 
 	// Sw/Sh Doubles
