0b52a9261ea93610c6d8c238b17dfd523601dc22
Quinn010 <quinnwilk@gmail.com>
2021-08-24 15:04:38 +0200

    l

--- config/formats.ts
+++ config/formats.ts
@@ -56,6 +56,23 @@ export const Formats: FormatList = [
 		banlist: [
 		'NU','OU','Porygon + Download', 'Trapinch + Arena Trap', 'Diglett + Arena Trap','UU'
 	]
+
+},
+{
+	name: "[Gen 8] LC NU",
+	threads: [
+		`&bullet; <a href="https://www.smogon.com/forums/threads/3656348/">LC Metagame Discussion</a>`,
+		`&bullet; <a href="https://www.smogon.com/forums/threads/3661419/">LC Sample Teams</a>`,
+		`&bullet; <a href="https://www.smogon.com/forums/threads/3657374/">LC Viability Rankings</a>`,
+
+	],
+
+	mod: 'gen8',
+	maxLevel: 5,
+	ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Switch Priority Clause Mod'],
+	banlist: [
+	'NU','OU','Porygon + Download', 'Trapinch + Arena Trap', 'Diglett + Arena Trap','UU', 'PU'
+]
 },
 
 	// Sw/Sh Doubles
